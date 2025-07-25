ad1073fda4d6bb7f90b06476bb00a3213cc8eb10
Full-Stack-Atok <komsaykerby@gmail.com>
2025-07-25 17:21:07 +0800

    Day 9 - Still Learning Chapter 6 will still do act. every section of the chapter.

--- notes/chapter6.js
+++ notes/chapter6.js
@@ -115,22 +115,146 @@ let blackRabbit = new Rabbit("black");
 // console.log({ x: 1 }.hasOwnProperty("x"));
 // console.log({ x: 1 }.hasOwnProperty("toString"));
 
-Rabbit.prototype.toString = function () {
-  return `a ${this.type} rabbit`;
-};
+// Rabbit.prototype.toString = function () {
+//   return `a ${this.type} rabbit`;
+// };
 
-console.log(String(blackRabbit));
+// console.log(String(blackRabbit));
 
-let sym = Symbol("name");
-console.log(sym == Symbol("name"));
+// let sym = Symbol("name");
+// console.log(sym == Symbol("name"));
 
-Rabbit.prototype[sym] = 55;
-console.log(blackRabbit[sym]);
+// Rabbit.prototype[sym] = 55;
+// console.log(blackRabbit[sym]);
 
-const toStringSymbol = Symbol("toString");
-Array.prototype[toStringSymbol] = function () {
-  return `${this.length} cm of blue yarn`;
-};
+// const toStringSymbol = Symbol("toString");
+// Array.prototype[toStringSymbol] = function () {
+//   return `${this.length} cm of blue yarn`;
+// };
 
-console.log([1,2]. toString());
-console.log([1,2][toStringSymbol]());
+// console.log([1, 2].toString());
+// console.log([1, 2][toStringSymbol]());
+
+// let stringObject = {
+//   [toStringSymbol]() {
+//     return "a jute rope";
+//   },
+// };
+
+// console.log(stringObject[toStringSymbol]());
+
+// let okIterator = "OK"[Symbol.iterator]();
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+// class MatrixIterator {
+//   constructor(matrix) {
+//     this.x = 0;
+//     this.y = 0;
+//     this.matrix = matrix;
+//   }
+
+//   next() {
+//     if (this.y == this.matrix.height) return { done: true };
+
+//     let value = {
+//       x: this.x,
+//       y: this.y,
+//       value: this.matrix.get(this.x, this.y),
+//     };
+//     this.x++;
+
+//     if (this.x == this.matrix.width) {
+//       this.x = 0;
+//       this.y++;
+//     }
+//     return { value, done: false };
+//   }
+// }
+
+// Matrix.prototype[Symbol.iterator] = function () {
+//   return new MatrixIterator(this);
+// };
+
+// let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+// for (let { x, y, value } of matrix) {
+//   console.log(x, y, value);
+// }
+
+// let varyingSize = {
+//   get size() {
+//     return Math.floor(Math.random() * 100);
+//   },
+// };
+
+// console.log(varyingSize.size);
+// console.log(varyingSize.size);
+
+// class Temperature {
+//   constructor(celsius) {
+//     this.celsius = celsius;
+//   }
+
+//   get fahrenheit() {
+//     return this.celsius * 1.8 + 32;
+//   }
+
+//   set fahrenheit(value) {
+//     this.celsius = (value - 32) / 1.8;
+//   }
+
+//   static fromFahrenheit(value) {
+//     return new Temperature((value - 32) / 1.8);
+//   }
+// }
+
+// let temp = new Temperature(22);
+// console.log(temp.fahrenheit);
+// temp.fahrenheit = 86;
+// console.log(temp.celsius);
+
+class SymmetricMatrix extends Matrix {
+  constructor(size, element = (x, y) => undefined) {
+    super(size, size, (x, y) => {
+      if (x < y) return element(y, x);
+      else return element(x, y);
+    });
+  }
+
+  set(x, y, value) {
+    super.set(x, y, value);
+    if (x != y) {
+      super.set(y, x, value);
+    }
+  }
+}
+
+let matrix = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
+console.log(matrix.get(2, 3));
+
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+console.log(new SymmetricMatrix(2) instanceof Matrix);
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
+console.log([1] instanceof Array);
